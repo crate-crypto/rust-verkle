19a961d07894c01cd13b292caae5cfe61c973c4a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-04-13 16:15:32 +0100

    transcipt
    - Add a basic transcript method that emulates the python and golang implementation in performance

--- src/transcript.rs
+++ src/transcript.rs
@@ -41,3 +41,56 @@ impl<E: PairingEngine> TranscriptProtocol<E> for Transcript {
         self.append_u64(b"n", n);
     }
 }
+
+// This transcript is used for performance comparisons with the python and golang implementation
+// It is not interopable because the python and golang implementation, do not separate domains
+pub struct BasicTranscript {
+    state: Vec<u8>,
+}
+impl BasicTranscript {
+    pub fn new(label: &'static [u8]) -> BasicTranscript {
+        let mut state = Vec::new();
+        state.extend(label);
+        BasicTranscript { state }
+    }
+
+    pub fn append_message(&mut self, label: &'static [u8], message: &[u8]) {
+        self.state.extend(label);
+        self.state.extend(message);
+    }
+    pub fn append_u64(&mut self, label: &'static [u8], x: u64) {
+        self.state.extend(label);
+        self.state.extend(&x.to_be_bytes());
+    }
+}
+
+impl<E: PairingEngine> TranscriptProtocol<E> for BasicTranscript {
+    fn append_point(&mut self, label: &'static [u8], point: &E::G1Affine) {
+        let bytes = to_bytes!(point).unwrap();
+
+        self.append_message(label, &bytes);
+    }
+
+    fn append_scalar(&mut self, label: &'static [u8], s: &E::Fr) {
+        let bytes = to_bytes!(s).unwrap();
+        self.append_message(label, &bytes)
+    }
+    // Python code essentially takes the sha256 hash
+    // then reduces it modulo the field
+    fn challenge_scalar(&mut self, label: &'static [u8]) -> E::Fr {
+        use sha2::Digest;
+
+        self.state.extend(label);
+
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&self.state);
+        let bytes = hasher.finalize();
+
+        E::Fr::from_be_bytes_mod_order(&bytes)
+    }
+
+    fn circuit_domain_sep(&mut self, n: u64) {
+        self.append_message(b"dom-sep", b"circuit_size");
+        self.append_u64(b"n", n);
+    }
+}
