e5ce2fac565ee50508a89d2c793e4d8f76a89a0a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-04-13 15:19:07 +0100

    trie
    - add more helper methods and make EmptyNode a ZST enum variant

--- src/trie/node/mod.rs
+++ src/trie/node/mod.rs
@@ -1,16 +1,17 @@
-use self::{empty::EmptyNode, hashed::HashedNode, internal::InternalNode, leaf::LeafNode};
+use self::{hashed::HashedNode, internal::InternalNode, leaf::LeafNode};
 
+pub(crate) mod children;
 pub mod empty;
 pub mod errors;
 pub mod hashed;
 pub mod internal;
 pub mod leaf;
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub enum Node {
     Internal(InternalNode),
     Hashed(HashedNode),
     Leaf(LeafNode),
-    Empty(EmptyNode),
+    Empty,
 }
 
 pub const EMPTY_NODE_TYPE: u8 = 0;
@@ -21,14 +22,31 @@ pub const HASHED_NODE_TYPE: u8 = 3;
 impl Node {
     pub const fn node_type(&self) -> u8 {
         match self {
-            Node::Empty(_) => EMPTY_NODE_TYPE,
+            Node::Empty => EMPTY_NODE_TYPE,
             Node::Leaf(_) => LEAF_NODE_TYPE,
             Node::Internal(_) => INTERNAL_NODE_TYPE,
             Node::Hashed(_) => HASHED_NODE_TYPE,
         }
     }
-
+}
+impl Node {
+    // XXX: Investigate a better way of doing the as conversions
     pub fn internal(self) -> InternalNode {
+        if let Node::Internal(internal) = self {
+            internal
+        } else {
+            panic!("not an internal node")
+        }
+    }
+    pub fn as_internal(&self) -> &InternalNode {
+        if let Node::Internal(internal) = self {
+            internal
+        } else {
+            panic!("not an internal node")
+        }
+    }
+
+    pub fn as_mut_internal(&mut self) -> &mut InternalNode {
         if let Node::Internal(internal) = self {
             internal
         } else {
@@ -46,6 +64,6 @@ impl Node {
 
 impl Default for Node {
     fn default() -> Node {
-        Node::Empty(EmptyNode)
+        Node::Empty
     }
 }
