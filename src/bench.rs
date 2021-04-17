ed30a8a878967eec82634a0dc651f1fbbf922020
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-04-17 20:04:05 +0100

    bench
    - remove other benchmarks, we only care about benching 2^24 on this branch

--- src/bench.rs
+++ src/bench.rs
@@ -46,71 +46,8 @@ static SRS: Lazy<(CommitKey<Bls12_381>, OpeningKey<Bls12_381>)> = Lazy::new(|| d
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{test::Bencher, verkle};
+    use crate::test::Bencher;
 
-    #[bench]
-    fn bench_insert_10k_from_1_million(b: &mut Bencher) {
-        let mut trie = setup_trie(1_000_000, &SRS.0);
-
-        let to_insert = generate_set_of_keys(10_000);
-
-        b.iter(|| insert_to_trie(&mut trie, &to_insert, &SRS.0))
-    }
-
-    #[bench]
-    fn bench_verify(b: &mut Bencher) {
-        let mut trie = VerkleTrie::new(10);
-        // insert 0 and 1, so that the proof is the longest it can be
-        let zero = Key::from_arr([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0,
-        ]);
-
-        let x = Key::from_arr([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0,
-        ]);
-
-        trie.insert(x, Value::max());
-        trie.insert(zero, Value::max());
-
-        let mut verkle_path = trie.create_path(&x, &SRS.0).unwrap();
-        let verkle_path2 = trie.create_path(&zero, &SRS.0).unwrap();
-
-        verkle_path = verkle_path.merge(verkle_path2);
-        let verkle_proof = verkle_path.create_proof(&SRS.0);
-
-        b.iter(|| {
-            let ok = verkle_proof.verify(
-                &SRS.1,
-                &verkle_path.commitments,
-                &verkle_path.omega_path_indices,
-                &verkle_path.node_roots,
-            );
-            assert!(ok);
-        })
-    }
-    #[bench]
-    fn bench_create_proof(b: &mut Bencher) {
-        let mut trie = VerkleTrie::new(10);
-        // insert 0 and 1, so that the proof is the longest it can be
-        let zero = Key::from_arr([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0,
-        ]);
-
-        let x = Key::from_arr([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0,
-        ]);
-
-        trie.insert(x, Value::max());
-        trie.insert(zero, Value::max());
-
-        let verkle_path = trie.create_path(&x, &SRS.0).unwrap();
-        dbg!(verkle_path.omega_path_indices.len());
-        b.iter(|| verkle_path.create_proof(&SRS.0))
-    }
     #[bench]
     fn bench_create_proof_224(b: &mut Bencher) {
         // benchmark the time it takes to create a proof where we have inserted 2^24 leaves
