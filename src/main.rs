0ca4d9365a7e39cc1d2e31b2664784d50f972797
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-05-06 21:02:27 +0100

    example
    - modify API to match new verkle trie implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use verkle_trie::{dummy_setup, Key, Value, VerkleTrie};
+use verkle_trie::{dummy_setup, Key, Value, VerkleTrait, VerkleTrie};
 fn main() {
-    let mut trie = VerkleTrie::new(10);
-
     println!("creating trusted setup");
-    let (commit_key, opening_key) = dummy_setup(1023);
+    let (commit_key, opening_key) = dummy_setup(10);
+
+    let mut trie = VerkleTrie::new(10, &commit_key);
 
     println!("creating inserting values");
-    trie.insert(Key::one(), Value::one());
-    trie.insert(Key::from_arr([1; 32]), Value::one());
+    trie.insert_single(Key::one(), Value::one());
+    trie.insert_single(Key::from_arr([1; 32]), Value::one());
 
     println!("creating verkle path");
-    let verkle_path = trie.create_path(&Key::one(), &commit_key).unwrap();
+    let verkle_path = trie.create_verkle_path(&Key::one()).unwrap();
 
     println!("creating verkle proof");
     let verkle_proof = verkle_path.create_proof(&commit_key);
