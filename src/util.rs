0a058bc56a33831faa91661d502f1098e5904b0d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-05-16 22:21:00 +0100

    util
    -move slow_powers into test module
    - conditionally compile tests

--- src/util.rs
+++ src/util.rs
@@ -2,15 +2,6 @@ use ark_ec::PairingEngine;
 use ark_ff::PrimeField;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-/// Returns a vector of Frs of increasing powers of x from x^0 to x^d.
-fn slow_powers_of<F: PrimeField>(scalar: &F, max_degree: usize) -> Vec<F> {
-    let mut powers = Vec::with_capacity(max_degree + 1);
-    powers.push(F::one());
-    for i in 1..=max_degree {
-        powers.push(powers[i - 1] * scalar);
-    }
-    powers
-}
 use std::iter::successors;
 pub(crate) fn powers_of<F: PrimeField>(scalar: &F, max_degree: usize) -> Vec<F> {
     powers_of_iter(*scalar, max_degree).collect::<Vec<_>>()
@@ -44,16 +35,6 @@ pub(crate) fn slow_multiscalar_mul_single_base<E: PairingEngine>(
         .collect()
 }
 
-#[test]
-fn powers_of_sucessor() {
-    let scalar = ark_bls12_381::Fr::from(2 as u128);
-    let powers = slow_powers_of(&scalar, 10);
-    assert_eq!(powers.len(), 11);
-    let powers2 = powers_of(&scalar, 10);
-    assert_eq!(powers2.len(), 11);
-    assert_eq!(powers, powers2)
-}
-
 #[cfg(test)]
 mod test {
     use crate::util;
@@ -61,6 +42,26 @@ mod test {
     use super::*;
     use ark_bls12_381::Fr;
     use ark_ff::Field;
+
+    /// Returns a vector of Frs of increasing powers of x from x^0 to x^d.
+    fn slow_powers_of<F: PrimeField>(scalar: &F, max_degree: usize) -> Vec<F> {
+        let mut powers = Vec::with_capacity(max_degree + 1);
+        powers.push(F::one());
+        for i in 1..=max_degree {
+            powers.push(powers[i - 1] * scalar);
+        }
+        powers
+    }
+    #[test]
+    fn powers_of_sucessor() {
+        let scalar = ark_bls12_381::Fr::from(2 as u128);
+        let powers = slow_powers_of(&scalar, 10);
+        assert_eq!(powers.len(), 11);
+        let powers2 = powers_of(&scalar, 10);
+        assert_eq!(powers2.len(), 11);
+        assert_eq!(powers, powers2)
+    }
+
     #[test]
     fn test_powers_of() {
         let x = Fr::from(10u64);
