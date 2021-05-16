7737ba4d338cd006b4b4d129d702ae5048ce567a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-05-16 19:03:04 +0100

    Hash
    - change Hash to be an array, and use Option where it is None

--- src/hash.rs
+++ src/hash.rs
@@ -4,22 +4,23 @@ use ark_bls12_381::Fr;
 use ark_ff::PrimeField;
 use std::convert::TryInto;
 
-#[derive(Debug, Clone, Copy)]
-pub enum Hash {
-    NotComputed,
-    Computed([u8; 32]),
+pub trait Hashable {
+    fn to_hash(&self) -> Hash;
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct Hash(pub [u8; 32]);
 
 impl Hash {
     pub const fn zero() -> Hash {
-        Hash::Computed([0u8; 32])
+        Hash([0u8; 32])
     }
 
     /// Converts a vector into an array
     /// This method will panic, the vector's length
     /// is not equal to the array's
     pub fn from_vec(v: Vec<u8>) -> Hash {
-        Hash::Computed(v.try_into().unwrap())
+        Hash(v.try_into().unwrap())
     }
 
     /// Converts a vector into an array
@@ -32,23 +33,17 @@ impl Hash {
         hasher.update(leaf.value.as_bytes());
 
         let res: [u8; 32] = hasher.finalize().try_into().unwrap();
-        Hash::Computed(res)
+        Hash(res)
     }
 
     // XXX: Fix to use a hash function and then reduce
     // This function will panic, if a Hash is not computed
     pub fn to_fr(&self) -> Fr {
-        match self {
-            Hash::NotComputed => panic!("cannot call hash_to_fr if the hash value is not computed"),
-            Hash::Computed(bytes) => Fr::from_le_bytes_mod_order(bytes),
-        }
+        Fr::from_le_bytes_mod_order(&self.0)
     }
     // This panics if the hash is not computed
     // This function is used for tests
     pub fn to_hex(&self) -> String {
-        match self {
-            Hash::NotComputed => panic!("hash not computed, cannot convert to hex"),
-            Hash::Computed(bytes) => hex::encode(bytes),
-        }
+        hex::encode(&self.0)
     }
 }
