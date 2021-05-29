cc37319af6573941c00a26db68dd93be958fb6be
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-05-29 15:16:01 +0100

    verkle_interop
    - Modify the PRNG to be simpler
    - Modify the test vectors to match the new PRNG

--- src/interop/verkle_interop.rs
+++ src/interop/verkle_interop.rs
@@ -100,63 +100,60 @@ mod test {
         assert_eq!("adb36cefe99f0931b017eaffbf3fc58e785bd92181fba6d15b015f203c0cf73a8b5a14baf68d4ecd0151012bec70367c",hex::encode(root.compress()))
     }
 
-    // The following step test cases were generated from the following python code:
-    //
-    // root = {"node_type": "inner", "commitment": blst.G1().mult(0)}
-    // add_node_hash(root)
-    //    state = int(0).to_bytes(32, "little")
-    //     for i in range(N):
-    //         new_state = hash(state)
-    //         state = new_state
-    //         key = state
-    //         value = int(0).to_bytes(32, "big")
-    //         insert_verkle_node(root, key, value)
-    //     add_node_hash(root)
-    //     print(bytes(root["commitment"].compress()).hex())
-    //
-    // Insert N keys from a prng, then compute the root.
-    // start at N and we increment by N
-
     #[test]
     fn test_vector_insert_100_step() {
         // N = 100
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 100, "b7bbeaa9457095d69ac9bb669bc63175784dcac6dc775f92d45e2180eddcb682baae640fa92a8e96c8aa1e2707908ff1");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 100, "8d8fe2e7769b498ec9bfda5da692901315db60e973ff40f75069a18b8508fa56b263b365b48980b2f874706ed2f34c73");
         // N = 200
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 200, "a91f655e75b85f2d60eaede73068af1061d32997c0651748d162a3a08ff6f6cdef9b0c6d8c843c50404e38c97351d7d3");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 200, "b018e472c8a12aec18d5d5d820225340229ebcbae6d70b22be636cb84e8a9bc1769fc6e790ad00fe76e8cb96bb56b34a");
         // N = 300
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 300, "b4b8b2b8e811f72e8b2a3522729356e2e7fa68decf1f75de4bbae19272be9f1b15d85cff1510a1f4a4c202f80aab8c73");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 300, "94f5f41a1c9c82fcd1602913dfb8fd8f70c688cf77de389e4a9d94842cd1c67f3a972720245b1277ef5ee601ada90f8d");
         // N = 400
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 400, "b7801d52d642af27fb019547ea629fae0b87230e12067d2e13004a2d8c8205eea6a99345e2037ed960747fbaab596cf5");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 400, "89565323cffcfaf4e3d841301e39a556dacabe74499337dd463011b220f470c2a69a8b73a69cb48af9402d3ca10b2b8e");
         // N = 500
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 500, "929cc832762aca99520552d170b6c5a0c2532778a1347d37d2c82ee0fe3e26f7503cad3f041667feb0128129d190000c");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 500, "95341e9c6a0467dca0967e7d4d5edc7594ff4b5bfa320d13cdcf38b48916668dbb71ecc9a5165f5d47241613dfbdda33");
     }
 
     #[test]
     fn test_vector_step_1k() {
         // N = 1_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 1_000, "809743aa087f81c8ec5c9c59640c9104c6399f48470da9438ae2707f9b9adf243b1be5c09ad98fbfb1683535ff02c1dd");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 1_000, "b5c18f72b43e6cb078f9dcee5d81f2d2cb99fc1af36bbe22b9860fb18b66f95419ae240736d3aaa4cecdaa412d360b58");
         // N = 2_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 2_000, "abfbd9adfda7ee6a160cb9a1b1ab4066c0a6babad9589c2bc6c29c8e04e1abfb8505611686a1ea49f1ea89674a83d3d2");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 2_000, "94a027952de98f3741ce485e6fed34942a7ac37997e3bb56c4ca743c53ad2efbd7e0fb6dbab97a24cfa38fd272036fae");
         // N = 3_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 3_000, "83d37aeb005a799f7a11794ed2416e371bc7bd538d98c7ed4913ef4d40c38c8f77d111442ede5a7a776639043aad64f8");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 3_000, "8d2d9fd6952eb4e60af58b0e06ec4669309f3a4b78a011ad8318425c6edca3f81e34228666ca57d76af8744f27c2fdc7");
         // N = 4_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 4_000, "b798afc162c04f0265af03fdec632a50a44c59a3209898cd5fdf63c1d1151e507a2c567aee9f402af2afb0d7a4d5871e");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 4_000, "a32ca8e260da356e9240dc419ea0a514163e0f6114e021d919dfe7c36f62ecd984b3dcb2de757527aaf9bca45eaac541");
         // N = 5_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 5_000, "82cfe5eadbe2eee91abca654a35f8cbdb9cee97904cc063a8af28fe3a6de4bbbbeb9b759881633f9d0c03e21e81769dd");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 5_000, "801d45490730c801e4a5d0b5b301126967b2811cf293de7ac79eeb39d0671af7a4df72a87c6202cfa0f0888521719e9c");
     }
 
     #[test]
     fn test_vector_step_10k() {
         // N = 10_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 10_000, "ac6f80ecf262097837348a4170c4a6600d855ba02663859aceb1d3753f1228eef73d0a68be7df8b73d0509230c254c5c");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 10_000, "b9aac5cf1fade329028d40aca951a0e2b614ad3193c41058451ddfcd57d1cc8438b3796dd62bf7478af2e5adc1a70fe4");
         // N = 20_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 20_000, "8c218ca0347c459db9f66b3b795c52b94e0f93adb96e8f6e8e9201b07f870377d4c1286ac38f4cbc610f696cdb3cab05");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 20_000, "b95db153f389b05f9c939273ba004b13fd32816d0f81255261e1ace6ba0b31b04a597cbc4be175616bf194dcd4a05860");
         // N = 30_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 30_000, "92761241a3c6d1093a007c250ced3d57e5332be5d77b9060b8c68be70612677e8e43f21528a68730697e7d70ae3a5c01");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 30_000, "b025a4efdf79e87990165ac0006b2f5b2965e7262d197ab5d4cf6a8d67647926368e836b28dae637be507c931b1ac681");
         // N = 40_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 40_000, "a56b3c6a6c4f038e06f978fc2d5e278fba982a73cec0ee44b3210b11134a77f1ecf0fdf0625d1989b0d72ba78eb41148");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 40_000, "8378e5a7a7592be9b7bf009dec095314a9d01343087c3109bdb20a4b838812a1ab47bdbc4b8d6043aa1ef09e4e686465");
         // N = 50_000
-        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 50_000, "a3518f182895d97a51ddaea8351e1f11a33439ff3bebeeb24241d1a8acc91030264c960d49232f40e031c8b5671c286e");
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 50_000, "abfabaa890dd3a85e793d8c018525abbdab1e25c84ade3c9934708a7a290fddc2c715e87a9ce0d190571fcfeda4bff72");
+    }
+
+    #[test]
+    fn test_vector_step_100k() {
+        // N = 100_000
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 100_000, "8602e138ef96fa3bfc559e6333dc7737e9f0a10ddc760a92397d5d002570ccd4636fd484f91bf63ce253de5f1eabdd62");
+        // N = 200_000
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 200_000, "abd86ec6e91cb5a0b1085a093b574239f96540b7c9f0ad5a9c7922c808656dc67d09d208e2eaac3579d31b3ac25f2e98");
+        // N = 300_000
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 300_000, "877a2ccce7adafea43c30c4382c5cca2e50e4ac6ff24ede9a928082ed5278b612e61c93172f1bcb6ebb6addec52c0f03");
+        // N = 400_000
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 400_000, "b129344704fa3b529a0324c128ce3cd9e34bf984bdf9f3389158acff8b8e28ec538190a12ae8763cede58ae05aaf0cf0");
+        // N = 500_000
+        step_test_helper(&*PRECOMPUTED_TABLE_1024, WIDTH_BITS, 500_000, "ab1d17b8eb8edd8dd90c415c55b4c7232fdae2b960e01aa886695b47c40ac767061cbdd3c12c72f27d73c4c6c0f1dc80");
     }
 
     fn step_test_helper(
@@ -182,7 +179,8 @@ mod test {
     // that is easy to implement in both python, go and Rust
     // This is only used for tests
     struct BasicPRNG {
-        state: [u8; 32],
+        state: [u8; 64],
+        counter: u128,
     }
 
     impl Default for BasicPRNG {
@@ -196,7 +194,14 @@ mod test {
 
     impl BasicPRNG {
         pub fn new(seed: [u8; 32]) -> BasicPRNG {
-            BasicPRNG { state: seed }
+            let mut state = [0u8; 64];
+
+            let counter = 0u128;
+            let counter_arr32 = BasicPRNG::arr32(counter);
+            state[..32].clone_from_slice(&counter_arr32);
+            state[32..].clone_from_slice(&seed);
+
+            BasicPRNG { state, counter }
         }
 
         pub fn rand_bytes(&mut self) -> [u8; 32] {
@@ -209,13 +214,27 @@ mod test {
 
             let res: [u8; 32] = hasher.finalize().try_into().unwrap();
 
-            self.state = res;
+            self.update_state();
 
             res
         }
 
         pub fn rand_vec_bytes(&mut self, num_keys: usize) -> Vec<[u8; 32]> {
             (0..num_keys).map(|_| self.rand_bytes()).collect()
+        }
+
+        fn update_state(&mut self) {
+            // update counter and replace the first 16 bytes with the
+            // new counter value. The seed does not change.
+            self.counter += 1;
+            let counter_arr32 = BasicPRNG::arr32(self.counter);
+            self.state[..32].clone_from_slice(&counter_arr32);
+        }
+
+        fn arr32(num: u128) -> [u8; 32] {
+            let mut result = [0u8; 32];
+            result[..16].clone_from_slice(&num.to_le_bytes()[..]);
+            result
         }
     }
 }
