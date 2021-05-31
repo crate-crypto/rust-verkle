01d524a4065f40ac7e81d19a5f5387176b239ab1
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-05-31 18:16:32 +0100

    benchmark
    - remove commented code

--- benches/benchmark_main.rs
+++ benches/benchmark_main.rs
@@ -6,11 +6,3 @@ criterion_main! {
     benchmarks::edit_10k::benches,
     benchmarks::insert_10k::benches,
 }
-
-// #[cfg(not(feature = "async_futures"))]
-// pub mod async_measurement_overhead {
-//     use criterion::{criterion_group, Criterion};
-//     fn some_benchmark(_c: &mut Criterion) {}
-
-//     criterion_group!(benches, some_benchmark);
-// }
