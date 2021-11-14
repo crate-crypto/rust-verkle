0cba6c00f3a270d4ef08753398ee6924dc8a6f37
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2021-11-14 14:48:23 +0000

    add comment about to_bytes

--- verkle-trie/src/to_bytes.rs
+++ verkle-trie/src/to_bytes.rs
@@ -1,5 +1,8 @@
 use ark_serialize::CanonicalSerialize;
 use bandersnatch::{EdwardsAffine, EdwardsProjective, Fr};
+// XXX: The only things that need to be converted to bytes are Points and scalars
+// so maybe we can return a [u8;32] and avoid allocating
+// Then use this instead of ark_serialize in the codebase
 pub trait ToBytes {
     fn to_bytes(&self) -> Vec<u8>;
 }
