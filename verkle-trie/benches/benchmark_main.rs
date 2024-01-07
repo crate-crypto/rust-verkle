418c4590fd35a0f8034da14c765149f1791e21dd
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-01-07 22:15:00 +0000

    remove unused file in benchmarks

--- verkle-trie/benches/benchmark_main.rs
+++ verkle-trie/benches/benchmark_main.rs
@@ -2,7 +2,6 @@ use criterion::criterion_main;
 
 mod benchmarks;
 criterion_main! {
-    benchmarks::precompute_scalar_mul::benches,
     benchmarks::insert_10k::benches,
     // benchmarks::edit_10k::benches,
     benchmarks::proof_10k::benches,
--- verkle-trie/benches/benchmarks/mod.rs
+++ verkle-trie/benches/benchmarks/mod.rs
@@ -1,5 +1,4 @@
 pub mod edit_10k;
 pub mod insert_10k;
-pub mod precompute_scalar_mul;
 pub mod proof_10k;
 pub mod util;
