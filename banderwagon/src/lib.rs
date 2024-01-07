9bf2ed1a7e471c2f3cd2f1ab8bd7f5d08a1b6383
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-01-07 21:47:07 +0000

    change api to expose Fr, element and msm

--- banderwagon/src/lib.rs
+++ banderwagon/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod trait_impls;
 
-pub mod element;
-pub use bandersnatch::{Fq, Fr};
-pub use element::{multi_scalar_mul, Element};
+mod element;
+pub use element::{multi_scalar_mul, Element, Fr};
