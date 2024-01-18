95ed2486f1410e637e2e40dbd9eeb2f89471cf56
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-01-18 23:02:05 +0000

    hash64 uses commit_lagrange

--- verkle-spec/src/lib.rs
+++ verkle-spec/src/lib.rs
@@ -13,13 +13,17 @@ pub use ethereum_types::{H160, H256, U256};
 
 pub use code::Code;
 pub use header::Header;
+use ipa_multipoint::committer::{Committer, DefaultCommitter};
 pub use storage::Storage;
 use verkle_trie::constants::new_crs;
 
 // Used to hash the input in get_tree_key
 pub trait Hasher {
     fn hash64(bytes64: [u8; 64]) -> H256 {
-        hash64(bytes64)
+        // TODO: We should make this a part of the Hasher signature instead of
+        // TODO being inefficient here
+        let committer = DefaultCommitter::new(&new_crs().G);
+        hash64(&committer, bytes64)
     }
 
     fn chunk64(bytes64: [u8; 64]) -> [u128; 5] {
@@ -33,20 +37,9 @@ pub trait Hasher {
 // This is the default implementation for `pedersen_hash`
 // in the EIP. Since the EIP hashes 64 bytes (address32 + tree_index),
 // we just special case the method here to hash 64 bytes.
-pub fn hash64(bytes64: [u8; 64]) -> H256 {
-    use ipa_multipoint::committer::{Committer, DefaultCommitter};
-    use verkle_trie::Element;
-
-    // TODO: We should either make this a global or have it be passed in
-    // TODO: so that we don't create a new crs each time
-    let committer = DefaultCommitter::new(&new_crs().G);
-    let mut result = Element::zero();
-
-    let inputs = crate::util::chunk64(bytes64);
-
-    for (index, input) in inputs.into_iter().enumerate() {
-        result += committer.scalar_mul(input.into(), index);
-    }
+pub fn hash64(committer: &DefaultCommitter, bytes64: [u8; 64]) -> H256 {
+    let inputs = crate::util::chunk64(bytes64).map(|input| verkle_trie::Fr::from(input));
+    let result = committer.commit_lagrange(&inputs);
 
     // Reverse the endian of the byte array
     let mut output = result.to_bytes();
@@ -71,16 +64,18 @@ pub fn addr20_to_addr32(addr20: Address20) -> Address32 {
 
 #[test]
 fn smoke_test_hash64() {
+    let committer = DefaultCommitter::new(&new_crs().G);
+
     // Hash of all zeroes
     let all_zeroes = [0u8; 64];
-    let hash = hash64(all_zeroes);
+    let hash = hash64(&committer, all_zeroes);
     let expected =
         hex::decode("bf101a6e1c8e83c11bd203a582c7981b91097ec55cbd344ce09005c1f26d1922").unwrap();
     assert_eq!(hash, H256::from_slice(&expected));
 
     // Hash of all ones
     let all_ones = [1u8; 64];
-    let hash = hash64(all_ones);
+    let hash = hash64(&committer, all_ones);
     let expected =
         hex::decode("54427497ffbee0d2511e14ddaf3497e9b5e8438ff17974d06918e0e8ebe8b61a").unwrap();
     assert_eq!(hash, H256::from_slice(&expected));
