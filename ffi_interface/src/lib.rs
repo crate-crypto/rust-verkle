185ccf14678ae4d2277547e2f2615933f10553b5
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-01-28 22:23:39 +0000

    add context object

--- ffi_interface/src/lib.rs
+++ ffi_interface/src/lib.rs
@@ -13,6 +13,41 @@ use ipa_multipoint::lagrange_basis::{LagrangeBasis, PrecomputedWeights};
 use ipa_multipoint::multiproof::{MultiPoint, ProverQuery};
 use ipa_multipoint::transcript::Transcript;
 
+/// Context holds all of the necessary components needed for cryptographic operations
+/// in the Verkle Trie. This includes:
+/// - Updating the verkle trie
+/// - Generating proofs
+///
+/// This is useful for caching purposes, since the context can be reused for multiple
+/// function calls. More so because the Context is relatively expensive to create
+/// compared to making a function call.
+pub struct Context {
+    pub crs: CRS,
+    pub committer: DefaultCommitter,
+
+    pub precomputed_weights: PrecomputedWeights,
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Context {
+    pub fn new() -> Self {
+        let crs = CRS::default();
+        let committer = DefaultCommitter::new(&crs.G);
+        let precomputed_weights = PrecomputedWeights::new(256);
+
+        Self {
+            crs,
+            committer,
+            precomputed_weights,
+        }
+    }
+}
+
 /// A serialized uncompressed group element
 pub type CommitmentBytes = [u8; 64];
 
