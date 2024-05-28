5ba451893d65a6eae3e7d400c2358b74b5b6d024
JEAN GAL <jgatbkk@gmail.com>
2024-05-28 11:31:45 -0400

    Added verify_proof function to be called from C# bandersnatch repository

--- bindings/c/src/lib.rs
+++ bindings/c/src/lib.rs
@@ -1,10 +1,12 @@
-use ffi_interface::{get_tree_key_hash, Context, fr_from_le_bytes, deserialize_proof_query};
+use ffi_interface::{
+    deserialize_proof_query, deserialize_verifier_query, fr_from_le_bytes, get_tree_key_hash,
+    Context,
+};
 use ipa_multipoint::committer::{Committer, DefaultCommitter};
 use ipa_multipoint::crs::CRS;
 use ipa_multipoint::lagrange_basis::PrecomputedWeights;
-use ipa_multipoint::multiproof::{MultiPoint, ProverQuery};
+use ipa_multipoint::multiproof::{MultiPoint, MultiPointProof, ProverQuery, VerifierQuery};
 use ipa_multipoint::transcript::Transcript;
-
 
 #[no_mangle]
 pub extern "C" fn context_new() -> *mut Context {
@@ -22,9 +24,13 @@ pub extern "C" fn context_free(ctx: *mut Context) {
     }
 }
 
-
 #[no_mangle]
-pub extern "C" fn pedersen_hash(ctx: *mut Context, address: *const u8, tree_index_le: *const u8, out: *mut u8) {
+pub extern "C" fn pedersen_hash(
+    ctx: *mut Context,
+    address: *const u8,
+    tree_index_le: *const u8,
+    out: *mut u8,
+) {
     if ctx.is_null() || address.is_null() || tree_index_le.is_null() || out.is_null() {
         // TODO: We have ommited the error handling for null pointers at the moment.
         // TODO: Likely will panic in this case.
@@ -39,7 +45,11 @@ pub extern "C" fn pedersen_hash(ctx: *mut Context, address: *const u8, tree_inde
         (tree_index_slice, add_slice, ctx_ref)
     };
 
-    let hash = get_tree_key_hash(context, <[u8; 32]>::try_from(add).unwrap(), <[u8; 32]>::try_from(tree_index).unwrap());
+    let hash = get_tree_key_hash(
+        context,
+        <[u8; 32]>::try_from(add).unwrap(),
+        <[u8; 32]>::try_from(tree_index).unwrap(),
+    );
 
     unsafe {
         let commitment_data_slice = std::slice::from_raw_parts_mut(out, 32);
@@ -48,16 +58,18 @@ pub extern "C" fn pedersen_hash(ctx: *mut Context, address: *const u8, tree_inde
 }
 
 #[no_mangle]
-pub extern "C" fn multi_scalar_mul(ctx: *mut Context, scalars: *const u8, len: usize, out: *mut u8)
-{
-
+pub extern "C" fn multi_scalar_mul(
+    ctx: *mut Context,
+    scalars: *const u8,
+    len: usize,
+    out: *mut u8,
+) {
     let (scalar_slice, context) = unsafe {
         let scalar = std::slice::from_raw_parts(scalars, len);
         let ctx_ref = &*ctx;
 
         (scalar, ctx_ref)
     };
-
 
     let mut inputs = Vec::with_capacity(len);
     for chunk in scalar_slice.chunks_exact(32) {
@@ -71,12 +83,10 @@ pub extern "C" fn multi_scalar_mul(ctx: *mut Context, scalars: *const u8, len: u
         let commitment_data_slice = std::slice::from_raw_parts_mut(out, 32);
         commitment_data_slice.copy_from_slice(&hash);
     }
-
 }
 
 #[no_mangle]
-pub extern "C" fn create_proof(ctx: *mut Context, input: *const u8, len: usize, out: *mut u8)
-{
+pub extern "C" fn create_proof(ctx: *mut Context, input: *const u8, len: usize, out: *mut u8) {
     const CHUNK_SIZE: usize = 8257; // TODO: get this from ipa-multipoint
     const PROOF_SIZE: usize = 576; // TODO: get this from ipa-multipoint
 
@@ -122,4 +132,56 @@ pub extern "C" fn create_proof(ctx: *mut Context, input: *const u8, len: usize, 
         let commitment_data_slice = std::slice::from_raw_parts_mut(out, PROOF_SIZE);
         commitment_data_slice.copy_from_slice(&hash);
     }
+}
+
+#[no_mangle]
+pub extern "C" fn verify_proof(ctx: *mut Context, input: *const u8, len: usize) -> bool {
+    const CHUNK_SIZE: usize = 65; // TODO: get this from ipa-multipoint
+    const PROOF_SIZE: usize = 576; // TODO: get this from ipa-multipoint
+
+    let (proof_slice, verifier_queries_slices, context) = unsafe {
+        let input_slice = std::slice::from_raw_parts(input, len);
+
+        let (proof_slice, verifier_queries_slices) = input_slice.split_at(PROOF_SIZE);
+
+        let ctx_ref = &*ctx;
+
+        (proof_slice, verifier_queries_slices, ctx_ref)
+    };
+
+    let verifier_queries_bytes = verifier_queries_slices.chunks_exact(CHUNK_SIZE);
+    assert!(
+        verifier_queries_bytes.remainder().is_empty(),
+        "There should be no left over bytes when chunking the verifier queries"
+    );
+
+    let num_openings = verifier_queries_bytes.len() / CHUNK_SIZE;
+
+    // - Deserialize verifier queries
+    //
+
+    let mut verifier_queries: Vec<VerifierQuery> = Vec::with_capacity(num_openings);
+
+    for verifier_query_bytes in verifier_queries_bytes {
+        let verifier_query = deserialize_verifier_query(verifier_query_bytes);
+        verifier_queries.push(verifier_query);
+    }
+
+    // - Check proof
+    //
+
+    let proof = MultiPointProof::from_bytes(proof_slice, 256).unwrap();
+
+    let mut transcript = Transcript::new(b"verkle");
+
+    // TODO: This should not need to clone the CRS, but instead take a reference
+    let is_valid = MultiPointProof::check(
+        &proof,
+        &context.crs.clone(),
+        &context.precomputed_weights,
+        &verifier_queries,
+        &mut transcript,
+    );
+
+    return is_valid;
 }
