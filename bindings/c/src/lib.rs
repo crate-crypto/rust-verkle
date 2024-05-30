dce65423a54e29b03af2711ea300eebd75e69f51
JEAN GAL <jgatbkk@gmail.com>
2024-05-30 16:40:14 -0400

    Created new create_proof_uncompressed to work with uncompressed comitment in prover queries bytes

--- bindings/c/src/lib.rs
+++ bindings/c/src/lib.rs
@@ -1,6 +1,6 @@
 use ffi_interface::{
-    deserialize_proof_query, deserialize_verifier_query, fr_from_le_bytes, get_tree_key_hash,
-    Context,
+    deserialize_proof_query, deserialize_proof_query_uncompressed, deserialize_verifier_query,
+    fr_from_le_bytes, get_tree_key_hash, Context,
 };
 use ipa_multipoint::committer::{Committer, DefaultCommitter};
 use ipa_multipoint::crs::CRS;
@@ -111,6 +111,61 @@ pub extern "C" fn create_proof(ctx: *mut Context, input: *const u8, len: usize, 
 
     for proof_bytes in proofs_bytes {
         let prover_query = deserialize_proof_query(proof_bytes);
+        prover_queries.push(prover_query);
+    }
+
+    // - Create proofs
+    //
+
+    let mut transcript = Transcript::new(b"verkle");
+
+    let proof = MultiPoint::open(
+        // TODO: This should not need to clone the CRS, but instead take a reference
+        context.crs.clone(),
+        &context.precomputed_weights,
+        &mut transcript,
+        prover_queries,
+    );
+
+    let hash = proof.to_bytes().expect("cannot serialize proof");
+    unsafe {
+        let commitment_data_slice = std::slice::from_raw_parts_mut(out, PROOF_SIZE);
+        commitment_data_slice.copy_from_slice(&hash);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn create_proof_uncompressed(
+    ctx: *mut Context,
+    input: *const u8,
+    len: usize,
+    out: *mut u8,
+) {
+    // 8257 + 32 because first commitment is uncompressed as 64 bytes
+    const CHUNK_SIZE: usize = 8289; // TODO: get this from ipa-multipoint
+    const PROOF_SIZE: usize = 576; // TODO: get this from ipa-multipoint
+
+    let (scalar_slice, context) = unsafe {
+        let scalar = std::slice::from_raw_parts(input, len);
+        let ctx_ref = &*ctx;
+
+        (scalar, ctx_ref)
+    };
+
+    let num_openings = len / CHUNK_SIZE;
+
+    let proofs_bytes = scalar_slice.chunks_exact(CHUNK_SIZE);
+    assert!(
+        proofs_bytes.remainder().is_empty(),
+        "There should be no left over bytes when chunking the proof"
+    );
+
+    // - Deserialize proof queries
+    //
+    let mut prover_queries: Vec<ProverQuery> = Vec::with_capacity(num_openings);
+
+    for proof_bytes in proofs_bytes {
+        let prover_query = deserialize_proof_query_uncompressed(proof_bytes);
         prover_queries.push(prover_query);
     }
 
